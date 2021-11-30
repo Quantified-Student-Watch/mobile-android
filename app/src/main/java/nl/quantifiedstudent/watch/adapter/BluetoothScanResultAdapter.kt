bd0629f1843d360e1c67f4d7f81a7ea08a1cd582
Jelle Maas <typiqally@gmail.com>
2021-11-30 13:43:18 +0100

    Add Bluetooth scan result recyclerview adapter

---
+++ app/src/main/java/nl/quantifiedstudent/watch/adapter/BluetoothScanResultAdapter.kt
@@ -0,0 +1,37 @@
+package nl.quantifiedstudent.watch.adapter
+
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanResult
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import nl.quantifiedstudent.watch.databinding.BluetoothScanResultRowItemBinding
+
+class BluetoothScanResultAdapter(
+    private val items: List<ScanResult>,
+    private val onClick: ((device: BluetoothDevice?) -> Unit)
+) : RecyclerView.Adapter<BluetoothScanResultAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = BluetoothScanResultRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding, onClick)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        println("add")
+        holder.bind(items[position])
+    }
+
+    inner class ViewHolder(
+        private val binding: BluetoothScanResultRowItemBinding,
+        private val onClickListener: ((device: BluetoothDevice?) -> Unit)
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(result: ScanResult) {
+            binding.deviceName.text = result.device.name ?: "Unknown"
+            binding.macAddress.text = result.device.address
+            binding.signalStrength.text = "${result.rssi} dBm"
+            binding.root.setOnClickListener { onClickListener.invoke(result.device) }
+        }
+    }
+}
