93ec5abf54d829479e9b3d5b1b2ed6b11692ba8e
Jelle Maas <typiqally@gmail.com>
2021-11-30 13:40:49 +0100

    Add Bluetooth pair activity

---
+++ app/src/main/java/nl/quantifiedstudent/watch/activity/BluetoothPairActivity.kt
@@ -0,0 +1,12 @@
+package nl.quantifiedstudent.watch.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import nl.quantifiedstudent.watch.R
+
+class BluetoothPairActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth_pair)
+    }
+}
