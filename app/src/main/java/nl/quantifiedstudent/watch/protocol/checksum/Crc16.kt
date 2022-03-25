2d4565051cea832846ffe02d72ef02af157d2595
Jelle Maas <typiqally@gmail.com>
2022-03-25 10:04:07 +0100

    Remove unnecessary experimental unsigned types

--- app/src/main/java/nl/quantifiedstudent/watch/protocol/checksum/Crc16.kt
+++ app/src/main/java/nl/quantifiedstudent/watch/protocol/checksum/Crc16.kt
@@ -1,17 +1,7 @@
 package nl.quantifiedstudent.watch.protocol.checksum
 
-@ExperimentalUnsignedTypes
 class Crc16 : Crc {
-    override fun calculate(
-        poly: UInt,
-        init: UInt,
-        data: UByteArray,
-        offset: Int,
-        length: Int,
-        refIn: Boolean,
-        refOut: Boolean,
-        xorOut: UInt
-    ): UInt {
+    override fun calculate(poly: Int, init: Int, data: ByteArray, offset: Int, length: Int, refIn: Boolean, refOut: Boolean, xorOut: Int): Int {
         var crc = init
         var i = offset
 
@@ -21,7 +11,7 @@ class Crc16 : Crc {
             for (j in 0..7) {
                 val k = if (refIn) 7 - j else j
                 val bit = b.toInt() shr 7 - k and 1 == 1
-                val c15 = crc shr 15 and 1u == 1u
+                val c15 = crc shr 15 and 1 == 1
                 crc = crc shl 1
                 if (c15 xor bit) crc = crc xor poly
             }
@@ -29,8 +19,8 @@ class Crc16 : Crc {
             ++i
         }
         return when {
-            refOut -> Integer.reverse(crc.toInt()).toUInt() shr 16 xor xorOut
-            else -> crc xor xorOut and 0xFFFFu
+            refOut -> Integer.reverse(crc) shr 16 xor xorOut
+            else -> crc xor xorOut and 0xFFFF
         }
     }
 }
