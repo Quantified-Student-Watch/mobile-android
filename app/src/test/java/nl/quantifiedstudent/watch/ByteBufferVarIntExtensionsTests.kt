7fca02fdcffc7d225ff0c240d99c87f22f38c783
Jelle Maas <typiqally@gmail.com>
2022-04-28 13:25:34 +0200

    Assert that VarInt is not writing unnecessary bytes

--- app/src/test/java/nl/quantifiedstudent/watch/ByteBufferVarIntExtensionsTests.kt
+++ app/src/test/java/nl/quantifiedstudent/watch/ByteBufferVarIntExtensionsTests.kt
@@ -2,7 +2,8 @@ package nl.quantifiedstudent.watch
 
 import nl.quantifiedstudent.watch.binary.getVarUInt
 import nl.quantifiedstudent.watch.binary.putVarUInt
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -15,7 +16,11 @@ import java.util.stream.Stream
 class ByteBufferVarIntExtensionsTests {
     @ParameterizedTest
     @MethodSource("provideVarIntValues")
-    fun `Given an empty buffer and value, when put VarInt with value, then buffer contains the VarInt bytes`(value: UInt, expected: ByteArray) {
+    fun `Given an empty buffer and value, when put VarInt with value, then buffer contains the VarInt bytes`(
+        value: UInt,
+        expected: ByteArray,
+        expectedPosition: Int
+    ) {
         // Given
         val buffer = ByteBuffer.allocate(5)
 
@@ -23,13 +28,17 @@ class ByteBufferVarIntExtensionsTests {
         buffer.putVarUInt(value)
 
         // Then
-        assertTrue(buffer.position() > 0) // buffer is written to
+        assertEquals(expectedPosition, buffer.position())
         assertArrayEquals(expected, buffer.array())
     }
 
     @ParameterizedTest
     @MethodSource("provideVarIntValues")
-    fun `Given a filled buffer with VarInt, when read VarInt, then result is VarInt as UInt`(expected: UInt, value: ByteArray) {
+    fun `Given a filled buffer with VarInt, when read VarInt, then result is VarInt as UInt`(
+        expected: UInt,
+        value: ByteArray,
+        expectedPosition: Int
+    ) {
         // Given
         val buffer = ByteBuffer.wrap(value)
 
@@ -38,7 +47,7 @@ class ByteBufferVarIntExtensionsTests {
 
         // Then
         assertEquals(5, buffer.limit())
-        assertTrue(buffer.position() > 0) // buffer is read from
+        assertEquals(expectedPosition, buffer.position())
         assertEquals(expected, result)
     }
 
@@ -46,13 +55,13 @@ class ByteBufferVarIntExtensionsTests {
         @JvmStatic
         private fun provideVarIntValues(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(UInt.MAX_VALUE.toInt(), byteArrayOf(-1, -1, -1, -1, 15)),
-                Arguments.of(Int.MAX_VALUE, byteArrayOf(-1, -1, -1, -1, 7)),
-                Arguments.of(UShort.MAX_VALUE.toInt(), byteArrayOf(-1, -1, 3, 0, 0)),
-                Arguments.of(Short.MAX_VALUE.toInt(), byteArrayOf(-1, -1, 1, 0, 0)),
-                Arguments.of(UByte.MAX_VALUE.toInt(), byteArrayOf(-1, 1, 0, 0, 0)),
-                Arguments.of(Byte.MAX_VALUE.toInt(), byteArrayOf(127, 0, 0, 0, 0)),
-                Arguments.of(0, byteArrayOf(0, 0, 0, 0, 0)),
+                Arguments.of(UInt.MAX_VALUE.toInt(), byteArrayOf(-1, -1, -1, -1, 15), 5),
+                Arguments.of(Int.MAX_VALUE, byteArrayOf(-1, -1, -1, -1, 7), 5),
+                Arguments.of(UShort.MAX_VALUE.toInt(), byteArrayOf(-1, -1, 3, 0, 0), 3),
+                Arguments.of(Short.MAX_VALUE.toInt(), byteArrayOf(-1, -1, 1, 0, 0), 3),
+                Arguments.of(UByte.MAX_VALUE.toInt(), byteArrayOf(-1, 1, 0, 0, 0), 2),
+                Arguments.of(Byte.MAX_VALUE.toInt(), byteArrayOf(127, 0, 0, 0, 0), 1),
+                Arguments.of(0, byteArrayOf(0, 0, 0, 0, 0), 1),
             )
         }
     }
